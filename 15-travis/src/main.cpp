813df99215ca46799fdacb9b8e02afa77146f1e6
brun1n0s <crinitib@gmail.com>
2020-11-12 02:30:28 +0100

    cmake 13 package manager   ush

--- 15-travis/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "include/Reverse.h"
-int main(int argc, char *argv[])
-{
-   Reverse r;
-   std::string toRev = "Hello Google test!";
-   std::cout << r.reverse(toRev) << std::endl;
-   return 0;
-}
